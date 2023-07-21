23de77eaa10a6e0cdceba3f3adff4b256e458d2e
soadfreyle <soadfre@gmail.com>
2023-07-20 21:01:30 -0500

    board locations

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+const TURNS = {
+  x: "x",
+  o: "o",
+};
 
+const board = Array(9).fill(null);
 function App() {
-  return <h1>Tic tac toc</h1>;
+  return (
+    <main className="board">
+      <h1>Tic tac toc</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <div className="cell" key={index}>
+              <span className="cell_content">{index}</span>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;

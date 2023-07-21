89e86d3ecba73a41732532e75f9023ce0caf675c
soadfreyle <soadfre@gmail.com>
2023-07-20 22:43:18 -0500

    make status when the user clicks on each position.

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,25 +1,23 @@
+import { useState } from "react";
+
 import "./App.css";
 const TURNS = {
   x: "x",
   o: "o",
 };
 
-const board = Array(9).fill(null);
-
 const Square = ({ children, updateBoard, index }) => {
   return <div className="square">{children}</div>;
 };
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  console.log(board);
   return (
     <main className="board">
       <h1>Tic tac toc</h1>
       <section className="game">
         {board.map((_, index) => {
-          return (
-            <Square key={index} index={index}>
-              {index}
-            </Square>
-          );
+          return <Square key={index} index={index}></Square>;
         })}
       </section>
     </main>

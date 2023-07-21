efc79ac4af41fed13ab49f6d894a01802c097b2c
soadfreyle <soadfre@gmail.com>
2023-07-20 22:50:46 -0500

    give it an initial value, to show the move.

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -10,14 +10,28 @@ const Square = ({ children, updateBoard, index }) => {
   return <div className="square">{children}</div>;
 };
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState([
+    "x",
+    "x",
+    "x",
+    "o",
+    "o",
+    "o",
+    "x",
+    "o",
+    "x",
+  ]);
   console.log(board);
   return (
     <main className="board">
       <h1>Tic tac toc</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
         })}
       </section>
     </main>

c73d46edf45a66f32960a8224a67114b488046aa
soadfreyle <soadfre@gmail.com>
2023-07-21 19:47:06 -0500

    establish a winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -18,19 +18,56 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   //estado para saber el turno
   const [turn, setTurn] = useState(TURNS.x);
+  //null no a ganador y false como un empate
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si  X u O gano
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    //si no hay ganador
+    return null;
+  };
 
   const updateBoard = (index) => {
     //no sobreescribe en la posicion
-    if (board[index]) return;
+    if (board[index] || winner) return;
+    //actualizar el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+    //cambiar el turno
     const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
     setTurn(newTurn);
+    //revisar si hay un ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
   return (
     <main className="board">

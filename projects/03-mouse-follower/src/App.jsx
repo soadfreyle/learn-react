d43076016e3939a4c42e98585dbff2d9768a8bd0
soadfreyle <soadfre@gmail.com>
2023-07-31 19:52:29 -0500

    saving the position

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     console.log("effect", { enabled });
@@ -9,24 +10,28 @@ function App() {
     const handleMove = (event) => {
       const { clientX, clientY } = event;
       console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
     };
 
-    window.addEventListener("pointermove", handleMove);
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
   }, [enabled]);
+
   return (
     <main>
       <div
         style={{
           position: "absolute",
           backgroundColor: "#09f",
-          borderEndEndRadius: "50%",
+          borderRadius: "50%",
           opacity: 0.8,
           pointerEvents: "none",
-          left: -20,
-          top: -20,
-          width: 40,
-          height: 40,
-          transform: "translate(0px, 0px)",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
         }}
       />
       <button onClick={() => setEnabled(!enabled)}>

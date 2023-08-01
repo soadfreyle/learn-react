541fbf106124644f52459bd4e96c50e6d1141a3e
soadfreyle <soadfre@gmail.com>
2023-07-31 19:33:47 -0500

    control the pointer function

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h3>Proyecto #3</h3>;
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    console.log("effect", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+    };
+
+    window.addEventListener("pointermove", handleMove);
+  }, [enabled]);
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderEndEndRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: "translate(0px, 0px)",
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </main>
+  );
 }
 
 export default App;
